aacfa6bbf1418e204391f708aec9cc760d217211
mengfu188 <1242733702@qq.com>
2020-04-09 15:57:17 +0800

    Add ssd_on_video.py and ncnn/datareader.h

---
+++ Face_Detector_ncnn/ssd_on_video.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+
+#include "FaceDetector.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+    string param = "../model/faceDetector.param";
+    string bin = "../model/faceDetector.bin";
+    const int max_side = 320;
+
+    // slim or RFB
+    Detector detector(param, bin, false);
+    // retinaface
+    // Detector detector(param, bin, true);
+    Timer timer;
+    cv::VideoCapture cap(0);
+    cv::Mat img;
+    for	(;;){
+
+
+//        cv::Mat img = cv::imread(imgPath.c_str());
+        cap >> img;
+        // scale
+        float long_side = std::max(img.cols, img.rows);
+        float scale = max_side/long_side;
+        cv::Mat img_scale;
+        cv::Size size = cv::Size(img.cols*scale, img.rows*scale);
+        cv::resize(img, img_scale, cv::Size(img.cols*scale, img.rows*scale));
+
+        std::vector<bbox> boxes;
+
+        timer.tic();
+
+        detector.Detect(img_scale, boxes);
+        timer.toc("----total timer:");
+
+        // draw image
+        for (int j = 0; j < boxes.size(); ++j) {
+            cv::Rect rect(boxes[j].x1/scale, boxes[j].y1/scale, boxes[j].x2/scale - boxes[j].x1/scale, boxes[j].y2/scale - boxes[j].y1/scale);
+            cv::rectangle(img, rect, cv::Scalar(0, 0, 255), 1, 8, 0);
+            char test[80];
+            sprintf(test, "%f", boxes[j].s);
+
+            cv::putText(img, test, cv::Size((boxes[j].x1/scale), boxes[j].y1/scale), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(0, 255, 255));
+            cv::circle(img, cv::Point(boxes[j].point[0]._x / scale, boxes[j].point[0]._y / scale), 1, cv::Scalar(0, 0, 225), 4);
+            cv::circle(img, cv::Point(boxes[j].point[1]._x / scale, boxes[j].point[1]._y / scale), 1, cv::Scalar(0, 255, 225), 4);
+            cv::circle(img, cv::Point(boxes[j].point[2]._x / scale, boxes[j].point[2]._y / scale), 1, cv::Scalar(255, 0, 225), 4);
+            cv::circle(img, cv::Point(boxes[j].point[3]._x / scale, boxes[j].point[3]._y / scale), 1, cv::Scalar(0, 255, 0), 4);
+            cv::circle(img, cv::Point(boxes[j].point[4]._x / scale, boxes[j].point[4]._y / scale), 1, cv::Scalar(255, 0, 0), 4);
+        }
+//        cv::imwrite("test.png", img);
+        cv::imshow("show", img);
+        int ret = cv::waitKey(1);
+    }
+    return 0;
+}
+
